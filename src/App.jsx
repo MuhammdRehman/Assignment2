0ea63160a3bbfca4ed7b1394e147ea1b3a8608a1
MuhammdRehman <ammuhammadrehman@gmail.com>
2024-10-19 22:28:18 +0500

    Add SignUp component in project

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Signin from "./components/signin";
 import Profile from "./components/profile";
 import Dashboard from "./components/Dashboard";
+import Signup from "./components/signup";
 
 const App = () => {
   return (
@@ -12,7 +13,8 @@ const App = () => {
           <Route path="/signin" element={<Signin/>} />
           <Route path="/" element={<Dashboard />} />
           <Route path="/profile" element={<Profile/>} />
-          <Route path="/sign-up" element={<h1>Sign Up Page</h1>} />
+          <Route path="/sign-up" element={<Signup/>} />
+          <Route path="/dashboard" element={<Dashboard/>}/>
           <Route path="/blank" element={<h1>Blank Page</h1>} />
           <Route path="*" element={<h1>404: Page Not Found</h1>} />
         </Routes>
